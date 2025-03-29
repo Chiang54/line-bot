b7beb3683b62f589b19c8d00966c76acf2dff0c9
jd.cbl1205 <jd.cbl1205@gmail.com>
2025-03-29 05:49:01 +0000

    修正格式

--- routers/get_solar.py
+++ routers/get_solar.py
@@ -22,12 +22,12 @@ def get_solar_longitude(ephemeris, ts, date):
     longitude = ecliptic[1].degrees
     return longitude
 
-def get_solar_term(date_str):
+def get_solar_term(date):
     # 載入天文資料
     ephemeris = load('de421.bsp')
     ts = load.timescale()
 
-    date = datetime.strptime(date_str, "%Y-%m-%d")
+    # date = datetime.strptime(date_str, "%Y-%m-%d")
     lon = get_solar_longitude(ephemeris, ts, date)
     
     # 檢查是否有接近節氣角度（容許±1°誤差）
