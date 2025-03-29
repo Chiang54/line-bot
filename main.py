35fd15bd36f1e384df57b4c4d5ea0f19deb9928b
jd.cbl1205 <jd.cbl1205@gmail.com>
2025-03-29 05:02:53 +0000

    修正CORS

--- main.py
+++ main.py
@@ -7,7 +7,20 @@ from stockAPI import main as st  # 引入 stockAPI 模組
 from my_linebot import my_linebot
 
 app = FastAPI()
+# 設定允許來源（可根據實際情況限制）
+origins = [
+    # "*",  # 🚨 開放全部來源，如果你要安全建議改成前端網址
+    "https://5174-idx-test-1743212816787.cluster-3g4scxt2njdd6uovkqyfcabgo6.cloudworkstations.dev/", 
+]
 
+# 加入 CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # 哪些網域可以存取
+    allow_credentials=True,
+    allow_methods=["*"],            # 允許的 HTTP 方法
+    allow_headers=["*"],            # 允許的 request headers
+)
 # 引入路由
 app.include_router(router.router, prefix='/router', tags=['基本'])
 app.include_router(crawler.router, prefix='/crawler', tags=['爬蟲'])
