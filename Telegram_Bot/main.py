9909d0e373f0e413548a8414fcd0c6eafd23d466
jd.cbl1205 <jd.cbl1205@gmail.com>
2025-04-06 08:19:29 +0000

    更新天氣API

--- Telegram_Bot/main.py
+++ Telegram_Bot/main.py
@@ -15,6 +15,7 @@ TELEGRAM_API = f"https://api.telegram.org/bot{TOKEN}"
 @router.post("/webhook")
 async def webhook(req: Request):
     data = await req.json()
+
     if "message" in data:
         chat_id = data["message"]["chat"]["id"]
         text = data["message"].get("text", "")
@@ -27,6 +28,19 @@ async def webhook(req: Request):
             send_message(chat_id, "這裡是今日新聞頭條：\n1. FastAPI 機器人爆紅！")
         else:
             send_message(chat_id, f"你說的是：{text}")
+    
+    elif "inline_query" in data:
+        inline_query_id = data["inline_query"]["id"]
+        query = data["inline_query"]["query"]
+        results = [{
+            "type": "article",
+            "id": "1",
+            "title": f"搜尋結果：{query}",
+            "input_message_content": {
+                "message_text": f"你輸入的是：{query}"
+            }
+        }]
+        answer_inline_query(inline_query_id, results)
     return {"status": "ok"}
 
 
@@ -38,18 +52,45 @@ def send_message(chat_id, text):
     }
     requests.post(url, json=payload)
 
-
+# 開始按鈕設定
 def send_welcome(chat_id):
     url = f"{TELEGRAM_API}/sendMessage"
     payload = {
         "chat_id": chat_id,
-        "text": "👋 歡迎使用 CunRong Bot！請選擇功能 👇",
+        "text": "👋 歡迎使用 *村榮商店* 機器人！請選擇功能 👇",
         "reply_markup": {
             "keyboard": [
                 [{"text": "📡 查天氣"}, {"text": "📰 看新聞"}]
             ],
             "resize_keyboard": True,
             "one_time_keyboard": False
+        }
+    }
+    requests.post(url, json=payload)
+
+# inLine設定
+def answer_inline_query(inline_query_id, results):
+    url = f"{TELEGRAM_API}/answerInlineQuery"
+    payload = {
+        "inline_query_id": inline_query_id,
+        "results": results,
+        "cache_time": 1
+    }
+    requests.post(url, json=payload)
+
+# 取得使用者位置
+def send_location_request(chat_id):
+    url = f"{TELEGRAM_API}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": "請傳送你的位置，我會回覆你當地的天氣 ☁️",
+        "reply_markup": {
+            "keyboard": [[{
+                "text": "📍 傳送位置",
+                "request_location": True
+            }]],
+            "resize_keyboard": True,
+            "one_time_keyboard": True
         }
     }
     requests.post(url, json=payload)
